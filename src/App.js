83062fac67d3bbe630190a5b2a71022e12455bb3
Sanket Mahadik <sanketmahadik1999@gmail.com>
2024-03-19 13:05:50 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import data from './data.json';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const [paragraphs, setParagraphs] = useState([]);
+
+  const generateParagraphs = () => {
+    const amountInput = document.getElementById('amount').value;
+    const amount = parseInt(amountInput);
+
+    if (!isNaN(amount) && amount > 0 && amount <= data.paragraphs.length) {
+      const generatedParagraphs = data.paragraphs.slice(0, amount);
+      setParagraphs(generatedParagraphs);
+    } else {
+      alert('Please enter a valid number between 1 and ' + data.paragraphs.length);
+    }
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>TIRED OF BORING LOREM IPSUM?</h1>
+     <div className='input-para'>
+      <span>Paragraphs: </span>
+      <input type="number" name="amount" id="amount"></input>
+      <button type="button" class="btn btn-warning" onClick={generateParagraphs}>Generate</button>
+     </div>
+     <div className='paragraphs'>
+        {paragraphs.map((paragraph, index) => (
+          <p key={index}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   );
 }
